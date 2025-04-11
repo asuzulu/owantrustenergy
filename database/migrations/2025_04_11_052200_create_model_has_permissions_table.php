63b9d5fa954acc954a6e399705b8a9f5c2ab3eea
asuzulu <asuzulu3@gmail.com>
2025-04-11 06:24:40 +0100

    users can now be deleted from the Edit Profile page by managers

--- database/migrations/2025_04_11_052200_create_model_has_permissions_table.php
+++ database/migrations/2025_04_11_052200_create_model_has_permissions_table.php
@@ -12,8 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('model_has_permissions', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('permission_id');
+
+            $table->string('model_type');
+            $table->unsignedBigInteger('model_id');
+
+            $table->index(['model_id', 'model_type'], 'model_has_permissions_model_id_model_type_index');
+
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on('permissions')
+                ->onDelete('cascade');
+
+            $table->primary(['permission_id', 'model_id', 'model_type'],
+                'model_has_permissions_permission_model_type_primary');
         });
     }
 
